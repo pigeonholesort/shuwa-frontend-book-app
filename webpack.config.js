54093149f8cc3897f8b506d12c7ed24bd525ce47
pige <akatsukisatoshi@gmail.com>
2020-10-27 21:35:14 +0900

    6-2まで

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: path.resolve(__dirname,'src/app.ts'),
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)s$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+                '@babel/preset-env',
+                '@babel/preset-typescript'
+              ]
+          }
+        }
+      }
+    ]
+  },
+};
